3294e521b50ea7b40d5dafaa25fcde934acfd43c
johnnyjiongyu <143734204+johnnyjiongyu@users.noreply.github.com>
2023-11-07 19:27:25 -0500

    Update jsChallenges.js

--- js/jsChallenges.js
+++ js/jsChallenges.js
@@ -1,4 +1,4 @@
-function challeenge1 {
+function challenge1() {
     console.log("Function is being called!");
     let checkBox = document.getElementById("subscribe").checked;
     if (checkBox) {
@@ -6,5 +6,17 @@ function challeenge1 {
     } else {
         document.querySelector("#emailDiv").style.display = "none";
     }
-}
+};
+
+
+let varCheckbox = document.getElementById("subscribe");
+varCheckbox.addEventListener("click", function() {
+    if (varCheckbox.checked) {
+        document.querySelector("#emailDiv").style.display = "block";
+    } else {
+        document.querySelector("#emailDiv").style.display = "none";
+    }});
+
+
+
 
